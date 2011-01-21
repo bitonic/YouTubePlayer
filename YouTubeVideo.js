61e8e5ea647f9a5a3ee6609ebf781bb6134e8c08
Francesco <f@mazzo.li>
2011-01-21 21:17:12 +0000

    Created interface to pass functions to the object.

--- YouTubeVideo.js
+++ YouTubeVideo.js
@@ -22,6 +22,7 @@
 var YouTubeVideo = new Class({
     Extends: Swiff,
     Implements: Events,
+
     // Options
     options: {
         id: null,
@@ -38,10 +39,11 @@ var YouTubeVideo = new Class({
         enablejsapi: 1,
         version: 3
     }),
+
+    // Records if the player is ready or not
+    playerReady: false,
     
     initialize: function(options) {
-        var self = this;
-
         this.setOptions(options);
         options = this.options;
 
@@ -55,7 +57,10 @@ var YouTubeVideo = new Class({
             }, true);            
         }
 
-        // Set the player id if we have it
+        // Set the player id if we have it.
+        // Actually since now the youtube video is a class, it's
+        // pretty useless to have this functionality. I'll decide
+        // later whether to keep it or not.
         if (options.id) {
             swfUrl.setData({playerapiid: options.id}, true);
         }
@@ -63,9 +68,40 @@ var YouTubeVideo = new Class({
         this.parent(swfUrl, options);
 
         // Fire the "playerReady" events when the player is ready.
-        window.onYouTubePlayerReady = function(playerapiid) {
-            self.fireEvent('playerReady', playerapiid);
+        var self = this;
+        window.onYouTubePlayerReady = function() {
+            self.playerReady = true,
+            self.fireEvent('playerReady');
         }
     },
 
+    enqueueVideoAction: function(fn) {
+        // Pop the first argument, which would be the function
+        var args = Array.from(arguments);
+        args.shift();
+
+        if (this.playerReady) {
+            fn.apply(this.object, args);
+        } else {
+            // If the player isn't ready yet, add an event to fire the
+            // requested function with the requested arguments when it
+            // will be ready.
+            var self = this;
+            this.addEvent('playerReady', function() {
+                fn.apply(self.object, args);
+            });
+        }
+    },
+
+    playVideo: function() {
+        this.enqueueVideoAction(this.object.playVideo);
+    },
+
+    pauseVideo: function() {
+        this.enqueueVideoAction(this.object.pauseVideo);
+    },
+
+    seekTo: function(seconds, seekAhead) {
+        this.enqueueVideoAction(this.object.seekTo, seconds, seekAhead);
+    },
 });
