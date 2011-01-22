46aa6a4c33b80c627e0c449ffef9305a88f3ef91
Francesco <f@mazzo.li>
2011-01-22 18:23:51 +0000

    Added the other functions and event listening.
    After spending some time on the event listening thing I realized that it
    can be much easier.  Committing just to make sure to save working code.

--- YouTubeVideo.js
+++ YouTubeVideo.js
@@ -26,8 +26,15 @@ var YouTubeVideo = new Class({
     // Options
     options: {
         id: null,
-        quality: 'default',
+        suggestedQuality: 'default',
         embedded: true
+    },
+
+    // The events available, mapped to the api events
+    ytEvents: {
+        stateChange: 'onStateChange',
+        playbackQualityChange: 'onPlaybackQualityChange',
+        error: 'onError'
     },
     
     // The default URIs
@@ -73,17 +80,89 @@ var YouTubeVideo = new Class({
             self.playerReady = true,
             self.fireEvent('playerReady');
         }
+
+        // It is necessary to create a global function to handle the
+        // API events, since the addEventListener function accepts
+        // string function name as paramenter, and not actual
+        // functions.        
+        // HACKY CODE AHEAD
+        var YTEventsHandler = new Class({
+            listeners: new Object(),
+
+            initialize: function() {
+                for (var i in self.ytEvents) {
+                    this.listeners[i] = new Array();
+                }
+            },
+
+            addEvent: function(type, fn) {
+                this.listeners[type] = this.listeners[type].concat([fn]);
+            },
+            
+            fireEvent: function(type, arg) {
+                // Call all the events
+                for (var i = 0; i < this.listeners[type].length; i++) {
+                    this.listeners[type][i](arg);
+                }
+            }
+        });
+
+        window.ytEventsHandler = new YTEventsHandler();
+    },
+
+    addEvent: function(type, fn) {
+        if (this.ytEvents[type] != undefined) {
+            // If the event is one of the events provided by the API,
+            // add the event listener through the API function.
+            window.ytEventsHandler.addEvent(type, fn);
+
+            // Remember that one event can be listened at a time, so
+            // the last event added will be listened.
+            window.ytEventsHandlerListener = function(arg) {
+                window.ytEventsHandler.fireEvent(type, arg);
+            }
+
+            this.enqueueAction('addEventListener', this.ytEvents[type],
+                               'ytEventsHandlerListener');
+        } else if (this.playerReady && type == 'playerReady') {
+            // If the event is playerReady and the player is ready,
+            // simply execute the function.
+            fn();
+        } else {
+            // Otherwise, fall back to the default addEvent method.
+            Events.prototype.addEvent.call(this, type, fn);
+        }
+    },
+            
+    // This function enqueues functions to be executed when the player
+    // is ready
+    enqueueAction: function(fnName) {
+        // Pop the first argument, which is the function name
+        var args = Array.from(arguments);
+        args.shift();
+
+        if (this.playerReady) {
+            this.object[fnName].apply(this.object, args);
+        } else {
+            // If the player isn't ready yet, add an event to fire the
+            // requested function with the requested arguments when it
+            // will be ready.
+            var self = this;
+            this.addEvent('playerReady', function() {
+                self.enqueueAction.apply(self, [fnName].concat(args));
+            });
+        }
     },
 
     //--------------------------------------------------------------------------
     // Queueing functions
 
 
-    loadVideo: function(idOrUrl, seconds, play, quality) {
+    loadVideo: function(idOrUrl, seconds, play, suggestedQuality) {
         // If no quality is specified, use the one specified when
         // creating the object
-        if (!quality) {
-            quality = this.quality;
+        if (!suggestedQuality) {
+            suggestedQuality = this.suggestedQuality;
         }
 
         var fnName;
@@ -92,19 +171,19 @@ var YouTubeVideo = new Class({
         if (this.isId(idOrUrl)) {
             // If the play paramenter is not present, default to load
             if (play === undefined || play) {
-                this.object.loadVideoById(idOrUrl, seconds, quality);
+                this.object.loadVideoById(idOrUrl, seconds, suggestedQuality);
             } else {
-                this.object.cueVideoById(idOrUrl, seconds, quality);
+                this.object.cueVideoById(idOrUrl, seconds, suggestedQuality);
             }
         } else {
             if (play === undefined || play) {
-                this.object.loadVideoByUrl(idOrUrl, seconds, quality);
+                this.object.loadVideoByUrl(idOrUrl, seconds, suggestedQuality);
             } else {
-                this.object.cueVideoByUrl(idOrUrl, seconds, quality);
+                this.object.cueVideoByUrl(idOrUrl, seconds, suggestedQuality);
             }
 
             // Set the quality
-            this.setQuality(quality);
+            this.setQuality(suggestedQuality);
         }
     },
 
@@ -116,31 +195,109 @@ var YouTubeVideo = new Class({
     //--------------------------------------------------------------------------
     // Playback controls and player settings
 
-    play: function() {
-        this.object.playVideo();
+
+    // Playing videos
+
+    playVideo: function() {
+        this.enqueueAction('playVideo');
     },
 
-    pause: function() {
-        this.object.pauseVideo();
+    pauseVideo: function() {
+        this.enqueueAction('pauseVideo');
     },
 
-    stop: function() {
-        this.object.stopVideo();
+    stopVideo: function() {
+        this.enqueueAction('stopVideo');
     },
 
-    seek: function(seconds, seekAhead) {
-        this.object.seekTo(seconds, seekAhead);
+    seekTo: function(seconds, seekAhead) {
+        this.enqueueAction('seekTo', seconds, seekAhead);
     },
 
-    clear: function() {
-        this.object.clearVideo();
+    clearVideo: function() {
+        this.enqueueAction('clearVideo');
     },
 
-    //--------------------------------------------------------------------------
-    // Player volume
+
+    // Changing volume
+
+    mute: function() {
+        this.enqueueAction('mute');
+    },
+
+    unMute: function() {
+        this.enqueueAction('unMute');
+    },
     
+    isMuted: function() {
+        return this.object.isMuted();
+    },
+
+    setVolume: function(volume) {
+        this.enqueueAction('setVolume', volume);
+    },
+
+    getVolume: function() {
+        return this.object.getVolume();
+    },
+
+
+    // Changing player size
+
+    setSize: function(width, height) {
+        this.enqueueAction('setSize', width, height);
+    },
     
+
+    // Playback status
+
+    getBytesLoaded: function() {
+        return this.object.getVideoBytesLoaded();
+    },
+
+    getBytesTotal: function() {
+        return this.object.getVideoBytesTotal();
+    },
+
+    getPlayerState: function() {
+        return this.object.getPlayerState();
+    },
+
+    getCurrentTime: function() {
+        return this.object.getCurrentTime();
+    },
+
     
+    // Playback quality
+
+    getPlaybackQuality: function() {
+        return this.object.getPlaybackQuality();
+    },
+
+    setPlaybackQuality: function(suggestedQuality) {
+        this.suggestedQuality = suggestedQuality;
+        
+        this.enqueueAction('setPlaybackQuality', suggestedQuality);
+    },
+
+    getAvailableQualityLevels: function() {
+        return this.object.getAvailableQualityLevels();
+    },
+
+    // Video info
+
+    getDuration: function() {
+        return this.object.getDuration();
+    },
+
+    getVideoUrl: function() {
+        return this.object.getVideoUrl();
+    },
+
+    getVideoEmbedCode: function() {
+        return this.getVideoEmbedCode();
+    },
+
     //--------------------------------------------------------------------------
     // Utilities
 
